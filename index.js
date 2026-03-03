05b37e1714d82e86db2aba759815223e4f4dbcfc
Nitya-vadoni <130901300+Nitya-vadoni@users.noreply.github.com>
2026-03-03 21:53:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Devops,I am learning Jenkins...");
+  res.write("Hello Jenkins");
   res.end();
 });
 
